c41a5f3c1f269e686f66ba19561198518f8fe69b
Georgi Dimitrov <georgi@duglemmerdetaldrig.dk>
2019-11-29 10:38:06 +0100

    Setting up project

--- the-great-bible-of-hunger/Startup.cs
+++ the-great-bible-of-hunger/Startup.cs
@@ -17,20 +17,18 @@ namespace the_great_bible_of_hunger {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -38,7 +36,8 @@ namespace the_great_bible_of_hunger {
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
