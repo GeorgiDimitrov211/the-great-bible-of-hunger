f0b6765c1c4d8fc85934c5a21f5d70c993d70f67
johnnybrykner <vit.brykner@sallinggroup.com>
2019-12-05 13:45:36 +0100

    Copy models and data to the new api project

---
+++ api/Models/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace the_great_bible_of_hunger.Models {
+    public class Recipe {
+        public int RecipeID { get; set; }
+        public decimal Price { get; set; }
+        public int Time { get; set; }
+        public string ImageURL { get; set; }
+        public string Title { get; set; }
+        public int CuisineID { get; set; }
+        public Cuisine Cuisine { get; set; }
+        public int DishTypeID { get; set; }
+        public DishType DishType { get; set; }
+        public int RatingID { get; set; }
+        public Rating Rating { get; set; }
+        [NotMapped]
+        public ICollection<string> Description { get; set; }
+        public virtual ICollection<RecipeDiet> RecipeDiets { get; set; }
+        public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; }
+
+        public Recipe() { }
+        public Recipe(decimal price, int time, string imageUrl, string title, int cuisineID, int dishtypeID, int ratingID) {
+            Price = price;
+            Time = time;
+            ImageURL = imageUrl;
+            Title = title;
+            CuisineID = cuisineID;
+            DishTypeID = dishtypeID;
+            RatingID = ratingID;
+        }
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using api.Data;
 
 namespace api
 {
@@ -23,9 +24,9 @@ namespace api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddDbContext<GBOHContext>(options => options.UseSqlServer(Configuration.GetConnectionString("GBOHContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
