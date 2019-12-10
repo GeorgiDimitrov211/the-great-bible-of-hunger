10919dc501886d70fe526b89f04188ef48652bc8
Luka Gurovic <luka.gurovic98@gmail.com>
2019-12-10 20:11:48 +0100

    Fixed the display of ingredients page, Connected landingpage, ingredients page, recipes page. Missing page that I made today with specific recipes. Linking not the best way possible but you can change it if you have better option

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -9,13 +9,56 @@ let landingPage = document.getElementById('ldn-screen--js');
 let homePage = document.getElementById('home-page');
 let fixedNavigation = document.getElementById('recipe-listing-page');
 let recipeNavigation = document.getElementById('recipe-page');
-let ingPage = document.getElementById('ingredients-page')
+let ingredientsPage = document.getElementById('ingredients-page');
+
+// get the element with the displayed and delete it depending 
+// on the page
+// Delete class from all function
+function removeDisplayedClass() {
+    let displayedClass = document.querySelectorAll('.displayed');
+    displayedClass.forEach(cls => {
+        cls.classList.remove('displayed');
+    });
+}
+
 
 // Skip button
 let skipButton = document.getElementById('skip__button');
 skipButton.addEventListener('click', () => {
-    landingPage.classList.remove('displayed');
+    removeDisplayedClass();
+    ingredientsPage.classList.add('displayed');
+    fixedNavigation.classList.add('displayed');
+})
+
+// Recipes page button
+let recipeButton = document.getElementById('recipe__button');
+recipeButton.addEventListener('click', () => {
+    removeDisplayedClass();
     homePage.classList.add('displayed');
     fixedNavigation.classList.add('displayed');
     recipeNavigation.classList.add('displayed');
+});
+
+
+// Products button
+let productsButton = document.getElementById('products__button');
+productsButton.addEventListener('click', () => {
+    removeDisplayedClass();
+    ingredientsPage.classList.add('displayed');
+    fixedNavigation.classList.add('displayed');
+});
+
+
+// Let's cook button
+let letsButton = document.getElementById('lets-cook__button');
+letsButton.addEventListener('click', () => {
+    removeDisplayedClass();
+    fixedNavigation.classList.add('displayed');
+    recipeNavigation.classList.add('displayed');
+    
 })
+// We need to apply black color to the current link!
+
+
+
+
