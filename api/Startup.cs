e3e9d5211d9889d1155ecf923be20c1aea79cc8a
johnnybrykner <vit.brykner@sallinggroup.com>
2019-12-11 23:01:00 +0100

    Set up the api to feed ingredients
    As a WIP, this commit allows for actual database ingredients to be
    fetched and placed inside the screen.
    
    The id's should be remembered by localStorage and upon refresh they
    should get refetched - coming up.

--- api/Startup.cs
+++ api/Startup.cs
@@ -21,12 +21,24 @@ namespace api
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<GBOHContext>
                 (opt => opt.UseSqlServer(Configuration.GetConnectionString("GBOHContext")));
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:5500", "http://localhost:5501");
+                });
+            });
+
             services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson(opt => {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
@@ -35,6 +47,8 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(MyAllowSpecificOrigins); 
+
             app.UseMvc();
             
             if (!env.IsDevelopment())
--- frontend/js/ingredients.js
+++ frontend/js/ingredients.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const baseUrl = "http://localhost:5000/api";
+  let empty = false;
+
+  const ingredientsList = document.querySelector(".ingredients__list");
+  const errorMessage = document.querySelector(".ingredients__input-error");
 
   document.querySelector(".ingredients__submit-icon").addEventListener("click", () => {
     verifyInput();
   });
 
   document.querySelector("#ingredients__input-value").addEventListener("keydown", event => {
+    errorMessage.classList.remove("thrown");
     if (event.keyCode === 13) {
       verifyInput();
     }
@@ -14,36 +20,60 @@ document.addEventListener("DOMContentLoaded", () => {
     const savedIngredients = window.localStorage.getItem("saved-ingredients");
     if (savedIngredients) {
       console.log("Making an API call with", savedIngredients);
-      // fake api call
-      const rawJson = await fetch("/frontend/js/fake.json");
-      const actualData = await rawJson.json();
-      actualData.ingredients.forEach(ingredient => {
-        const outputIngredient = document.createElement("figure");
-        outputIngredient.classList.add("ingredients__list__ingredient");
-        outputIngredient.innerHTML = `
-          <img src="${ingredient.ImageURL}">
-          <figcaption>${ingredient.Name}</figcaption>
-          <span class="ingredients__list__ingredient__type">${ingredient.Type}</span>
-        `;
-        document.querySelector(".ingredients__list").appendChild(outputIngredient);
-      });
+
+      // const rawData = await fetch("/frontend/js/fake.json");
+      // const actualData = await rawData.json();
+      // actualData.ingredients.forEach(ingredient => {
+      //   renderIngredient(ingredient);
+      // });
       return;
     }
     else
     {
-      document.querySelector(".ingredients__list").innerHTML = "<h4>You have no ingredients saved, try adding some!</h4>";
+      empty = true;
+      ingredientsList.innerHTML = "<h5>You have no ingredients saved, try adding some!</h5>";
     };
   })();
 
-  async function submitIngredient(ingredientToBeSubmitted) {
-    console.log(ingredientToBeSubmitted);
+  async function fetchIngredient(ingredientQuery) {
+    const rawIngredient = await fetch(`${baseUrl}/ingredient/${ingredientQuery}`);
+    const actualIngredient = await rawIngredient.json();
+    if (actualIngredient && actualIngredient.status) {
+      errorMessage.classList.add("thrown");
+      return;
+    }
+    renderIngredient(actualIngredient);
   };
 
   function verifyInput() {
     const inputValue = document.querySelector("#ingredients__input-value").value;
     if (inputValue && inputValue.length > 1) {
-      submitIngredient(inputValue);
+      fetchIngredient(inputValue);
+    } else {
+      errorMessage.classList.add("thrown");
     }
   };
 
+  function renderIngredient(ingredient) {
+    if (empty = true) {
+      ingredientsList.innerHTML = "<h5>Your ingredients:</h5>";
+      empty = false;
+    }
+    const outputIngredient = document.createElement("figure");
+    outputIngredient.classList.add("ingredients__list__ingredient");
+    outputIngredient.innerHTML = `
+      <figcaption>${ingredient.name}</figcaption>
+      <div class="ingredients__list__ingredient__data">
+        <img src="${ingredient.imageURL}">
+        <div class="ingredients__list__ingredient__type">${ingredient.type}</div>
+      </div>
+    `;
+    ingredientsList.append(outputIngredient);
+    let savedIngredients = Array.from(window.localStorage.getItem("saved-ingredients"));
+    console.log("saved", savedIngredients);
+    if (!savedIngredients) savedIngredients = [];
+    savedIngredients.push(ingredient.ingredientId);
+    window.localStorage.setItem("saved-ingredients", savedIngredients);
+    console.log("updated", Array.from(window.localStorage.getItem("saved-ingredients")));
+  }
 });
