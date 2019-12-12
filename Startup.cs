5b0515e28fec507b2a4a5ec00839e18c57ee154d
Georgi Dimitrov <georgi@duglemmerdetaldrig.dk>
2019-12-12 16:43:26 +0100

    Add recipies and update Startup

--- Startup.cs
+++ Startup.cs
@@ -14,47 +14,49 @@ using api.Data;
 
 namespace api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext<GBOHContext>
-                (opt => opt.UseSqlServer(Configuration.GetConnectionString("GBOHContext")));
-            
-            services.AddCors(options =>
-            {
-                options.AddPolicy(MyAllowSpecificOrigins,
-                builder =>
-                {
-                    builder.WithOrigins("http://localhost:5500", "http://localhost:5501").AllowAnyHeader();
-                });
-            });
-
-            services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson(opt => {
-                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseCors(MyAllowSpecificOrigins); 
-
-            app.UseMvc();
-            
-            if (!env.IsDevelopment())
-            {
-                app.UseHsts();
-            }
-        }
+      Configuration = configuration;
     }
+
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<GBOHContext>
+          (opt => opt.UseSqlServer(Configuration.GetConnectionString("GBOHContext")));
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy(MyAllowSpecificOrigins,
+              builder =>
+              {
+            builder.WithOrigins("http://localhost:5500", "http://localhost:5501", "https://gboh.netlify.com").AllowAnyHeader();
+          });
+      });
+
+      services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson(opt =>
+      {
+        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseCors(MyAllowSpecificOrigins);
+
+      app.UseMvc();
+
+      if (!env.IsDevelopment())
+      {
+        app.UseHsts();
+      }
+    }
+  }
 }
